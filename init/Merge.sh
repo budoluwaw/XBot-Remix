fbf376c04502aaea11fd7a59ee13914bd49f72ae
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-29 16:33:25 +0700

    Update Merge.sh

--- init/Merge.sh
+++ init/Merge.sh
@@ -13,13 +13,13 @@ export SEMAPHORE_PROJECT_DIR=`pwd`
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
 TELEGRAM_TOKEN=${BOT_API_KEY}
 export BOT_API_KEY TELEGRAM_TOKEN
-tg_sendinfo "<code>I am gonna merge staging into x-sql-extended</code>"
+tg_sendinfo "<code>I am gonna merge staging into alpha</code>"
 cd
 git clone https://github.com/X-Newbie/XBot-Remix.git
 cd XBot-Remix
 git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com/X-Newbie/XBot-Remix.git
 git fetch
-git checkout staging
-git pull origin staging
-git push --force origin staging:x-sql-extended
+git checkout alpha
+git pull origin alpha
+git push --force origin staging:alpha
 tg_sendinfo "<code>I have merged all commits from staging into x-sql-extended</code>"
