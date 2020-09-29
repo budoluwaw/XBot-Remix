3951057c3a578ca69760501d6aecbe1a31c25c9e
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-29 07:57:14 +0700

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,8 @@ FROM alfianandaa/alf:groovy
 #
 # Clone repo and prepare working directory
 #
-RUN git clone -b x-sql-extended https://github.com/X-Newbie/XBot-Remix /root/userbot
-RUN mkdir /root/userbot/.bin
-WORKDIR /root/userbot
-
-#Install python requirements
-RUN pip3 install -r https://raw.githubusercontent.com/X-Newbie/XBot-Remix/x-sql-extended/requirements.txt
+RUN git clone -b master https://github.com/X-Newbie/XBot-Remix /home/projectalf/
+RUN mkdir /home/projectalf/bin/
+WORKDIR /home/projectalf/
 
 CMD ["python3","-m","userbot"]
--- string_session.py
+++ string_session.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python3
-# (c) https://t.me/TelethonChat/37677 and SpEcHiDe
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
 print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
-Create a new application, by entering the required details
-Check di Telegram Pesan Tersimpan untuk Melihat Kode STRING_SESSION""")
-API_KEY = int(input("Enter API_KEY here: "))
-API_HASH = input("Enter API_HASH here: ")
+Create a new application, by entering the required details""")
+
+API_KEY = input("API_KEY: ")
+API_HASH = input("API_HASH: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print("Check di Telegram Pesan Tersimpan untuk Melihat Kode STRING_SESSION")
     session_string = client.session.save()
-    saved_messages_template = """XBOT CHANNEL SUPPORT: @XBOT_SUPPORT
-
-<code>STRING_SESSION</code>: <code>{}</code>
-
-⚠️ <i>Silahkan Klick Code String Untuk Mengcopy</i>""".format(session_string)
+    saved_messages_template = """<code>STRING_SESSION</code>: <code>{}</code>
+️<i>It is forbidden to pass this value to third parties</i>""".format(session_string)
     client.send_message("me", saved_messages_template, parse_mode="html")
+    print("Check Saved Messages on your telegram account !!")
