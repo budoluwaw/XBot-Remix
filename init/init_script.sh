5af517f9e3292620f5c26df84970ca0ffb22561f
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-09-29 16:31:58 +0700

    Update init_script.sh

--- init/init_script.sh
+++ init/init_script.sh
@@ -48,7 +48,7 @@ botclone() {
     cd ~
     echo "Cloning bot sources..."
     if [ -z "$bot_clone" ]; then
-        git clone https://github.com/X-Newbie/XBot-Remix -b x-sql-extended
+        git clone https://github.com/X-Newbie/XBot-Remix -b alpha
         save_prog "bot_clone"
     fi
     echo "DONE!!"
